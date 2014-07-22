4426435cd5e3240a8e6a7fcdc7fa7bbbb7cdf93c
Jakub <kubatrt@gmail.com>
2014-07-23 01:18:18 +0200

    First game draft
    update gitignore, first game draft

---
+++ Assets/Dot.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class Dot : MonoBehaviour 
+{
+
+	/*public class EDotType 
+	{
+		public static readonly Color DT_Yellow = Color.yellow;
+		public static readonly Color DT_Green = Color.green;
+		public static readonly Color DT_Red = Color.red;
+		public static readonly Color DT_Blue = Color.blue;
+
+		public static int dotTypes = 4;
+
+	}*/
+
+	public static readonly Color[] dotColors = { Color.green, Color.red, Color.blue, Color.yellow };
+
+	DotsBoard			dotsBoard;
+	public bool			isSelected;
+	public bool 		isConnected;
+	public int 			type;
+	public Vector2		boardPosition;
+
+	//public Color		startColor = Color.white;
+	//public Color		targetColor;
+
+	Vector3 beginLine;
+
+	SpriteRenderer		srenderer;
+	LineRenderer		lrenderer;
+
+	// Use this for initialization
+	void Start () {
+		dotsBoard = GameObject.FindObjectOfType<DotsBoard>();
+		srenderer = GetComponent<SpriteRenderer>();
+		lrenderer = GetComponent<LineRenderer>();
+
+		type = Random.Range(0, dotColors.Length);
+		//targetColor = Color.white; //
+		srenderer.color = dotColors[type];
+		particleSystem.startColor = dotColors[type];
+
+		beginLine = new Vector3( transform.position.x,  transform.position.y, transform.position.z + 0.05f);
+		lrenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lrenderer.SetWidth(0.2f, 0.2f);
+		lrenderer.SetColors(dotColors[type], dotColors[type]);
+
+		isSelected = false;
+		isConnected = false;
+	}
+
+	void DrawLine(Vector3 endLine)
+	{
+		if(!lrenderer) 
+			return;
+
+
+		lrenderer.SetPosition(0, beginLine);
+		lrenderer.SetPosition(1, endLine);
+	}
+
+	void CheckNeighbors() {
+		Vector2 pos = Vector2.zero;
+		// right
+		if(boardPosition.x+1 < dotsBoard.boardWidth) {	
+			Dot dot = dotsBoard.GetDotByBoardPosition(boardPosition.x+1, boardPosition.y);
+			if(dot.isSelected && dot.type == type) {
+				isConnected = true;
+				dot.isConnected = true;
+				DrawLine(dot.transform.position);
+			}
+		}
+		// left
+		if(boardPosition.x-1 >= 0) { 
+			Dot dot = dotsBoard.GetDotByBoardPosition(boardPosition.x-1, boardPosition.y);
+			if(dot.isSelected && dot.type == type) {
+				isConnected = true;
+				dot.isConnected = true;
+				DrawLine(dot.transform.position);
+			}
+		}
+		// bottom
+		if(boardPosition.y+1 < dotsBoard.boardHeight) {
+			Dot dot = dotsBoard.GetDotByBoardPosition(boardPosition.x, boardPosition.y+1);
+			if(dot.isSelected && dot.type == type) {
+				isConnected = true;
+				dot.isConnected = true;
+				DrawLine(dot.transform.position);
+			}
+		}
+		// top
+		if(boardPosition.y-1 >= 0) {
+			Dot dot = dotsBoard.GetDotByBoardPosition(boardPosition.x, boardPosition.y-1);
+			if(dot.isSelected && dot.type == type) {
+				isConnected = true;
+				dot.isConnected = true;
+				DrawLine(dot.transform.position);
+			}
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(isSelected) {
+
+		} else {
+
+		}
+
+		if(isConnected) {
+			//lrenderer.enabled = false;
+
+		} else {
+			//lrenderer.enabled = true;
+			DrawLine(beginLine);
+		}
+
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		if(Input.GetMouseButton(0))
+		{
+			if(collider2D.OverlapPoint(mousePos))
+			{
+				if(!isSelected) {
+					isSelected = true;
+					particleSystem.Play();
+					CheckNeighbors();
+					dotsBoard.SetCurrentDot(this);
+				}
+
+			}
+		}
+	}
+
+}
---
+++ Assets/DotsBoard.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DotsBoard : MonoBehaviour {
+
+	public int 			boardWidth = 5;
+	public int 			boardHeight	= 5;
+	public float 		dotDistance = 1f;
+	public GameObject	dotSprite;
+
+	public Vector2[,]	boardPositions;
+	public List<Dot>	boardDots = new List<Dot>();
+
+	public List<Dot>	boardDotsSelected = new List<Dot>();
+	public Dot			previousSelection;
+	public Dot			currentSelection;
+
+	float startX;
+	float startY;
+
+	void Start () 
+	{
+		if(!dotSprite) {
+			Debug.LogError("There's no \"dot\" sprite attached!");
+			return;
+		}
+		InitializeBoard();
+	}
+
+	void Update () 
+	{
+		if(Input.GetMouseButton(0)) {
+
+		}
+		if(Input.GetMouseButtonDown(0)) {
+			
+		}
+		if(Input.GetMouseButtonUp(0)) {
+			ClearDots();
+			previousSelection = null;
+			currentSelection = null;
+		}
+	}
+
+	public void SetCurrentDot(Dot current) {
+		if(currentSelection != null) {
+			previousSelection = currentSelection;
+		}
+		currentSelection = current;
+	}
+
+	public Dot GetDotByBoardPosition(float x, float y)
+	{
+		Dot returnDot = new Dot();
+		foreach(Dot dot in boardDots) {
+			Vector2 pos = new Vector2(x,y);
+			if(dot.boardPosition == pos) {
+				returnDot = dot;
+				break;
+			}
+		}
+		return returnDot;
+	}
+	
+	public Vector3 BoardToWorldPosition(Vector2 pos) 
+	{
+		return BoardToWorldPosition(pos.x, pos.y);
+	}
+	
+	public Vector3 BoardToWorldPosition(float x, float y)
+	{
+		return new Vector3(x * dotDistance - startX , y * dotDistance - startY, 0f);
+	}
+
+	void ClearDots() {
+		Debug.Log ("ClearDots()");
+		List<Dot> removeList = new List<Dot>();
+		foreach(Dot dot in boardDots) {
+			if(dot.isConnected) {
+				removeList.Add(dot);
+
+			}
+		}
+
+		foreach(Dot dot in removeList) {
+			boardDots.Remove(dot);
+			Destroy (dot.gameObject);
+		}
+
+		foreach(Dot dot in boardDots) {
+			dot.isSelected = false;
+			//dot.isConnected = false;
+		}
+	}
+	
+	void InitializeBoard()
+	{
+		int dotsNumber = boardWidth * boardHeight;
+		boardPositions = new Vector2[boardWidth, boardHeight];
+		
+		startX = dotDistance * (boardWidth / 2); 
+		startY = dotDistance * (boardHeight / 2);
+		startX -= (boardWidth % 2) == 0 ? (dotDistance/2f) : 0f;
+		startY -= (boardHeight % 2) == 0 ? (dotDistance/2f): 0f;
+		
+		for(int y = 0; y < boardHeight; y++) {
+			for(int x = 0; x < boardWidth; x++) {
+				Vector2 position = new Vector2(x * dotDistance - startX, y * dotDistance - startY);
+				boardPositions[x,y] = position;
+				
+				Dot dot = ((GameObject)GameObject.Instantiate(dotSprite, new Vector3(position.x, position.y, 0f), Quaternion.identity)).GetComponent<Dot>();
+				dot.transform.parent = transform;
+				dot.boardPosition = new Vector2(x,y);
+				dot.name = dot.name + x*y;
+				boardDots.Add (dot);
+			}
+		}
+		
+		//Debug.Log(dotSprite.GetComponent<SpriteRenderer>().sprite.bounds);
+	}
+
+	void UpdateBoard()
+	{
+		// TODO:
+		// update board after players pull of the button
+		// remove conntected dots
+		// move dots to specific direction to fill empty spaces
+		// create new dots on empty positions
+	}
+}
